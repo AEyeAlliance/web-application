6a5037cf17ed197d26c3ee8c0054986e2c83ef60
Ting-Yi Su <tingyisu8@gmail.com>
2018-07-07 12:02:01 -0400

    delete unnecessary import statements and functions

--- final_model/space_recognition_original.py
+++ final_model/space_recognition_original.py
@@ -1,53 +1,9 @@
 import torch
 import torch.nn as nn
-# import torch.onnx
-# import onnx
-# import onnx_caffe2.backend
-# from onnx import checker, helper
-import torch.optim as optim
 import numpy as np
 import cv2
 from PIL import Image
-import torch.utils.model_zoo as model_zoo
-import torch.onnx
 
-
-def export_model():
-    model = CNN()
-    model.load_state_dict(torch.load("model.pth"))
-
-    # Input to the model
-    x = torch.randn(5, 3, 28, 28)
-
-    # Export the model
-    torch_out = torch.onnx._export(model,  # model being run
-                                   x,  # model input (or a tuple for multiple inputs)
-                                   "model.onnx-2",
-                                   # where to save the model (can be a file or file-like object)
-                                   export_params=True)  # store the trained parameter weights inside the model file
-
-def inspect_model():
-    # Input image into the ONNX model
-    onnx_model = onnx.load("model.onnx")
-    model = onnx_caffe2.backend.prepare(onnx_model)
-
-    image = Image.open("z.jpg")
-    # # image = image.convert('RGB')
-    image = np.array(image)
-    image = cv2.resize(image, (28, 28))
-    image = image.astype(np.float32) / 255.0
-    image = torch.from_numpy(image[None, :, :, :])
-    image = image.permute(0, 3, 1, 2)
-    W = {model.graph.input[0].name: image.data.numpy()}
-    model_out = model.run(W)[0]
-    print(model_out)
-    #
-    # # onnx_model(image)
-    #
-    # print(onnx_model)
-
-    # onnx.checker.check_model(onnx_model)
-    # # print(onnx.helper.printable_graph(onnx_model.graph))
 
 def make_prediction(img_path):
     model = CNN()
